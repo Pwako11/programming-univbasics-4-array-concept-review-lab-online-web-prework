b136d024605265222fa8e75bf5f1dbfea11ce8aa
Patrick A <akolo@hotmail.com>
2020-01-20 19:58:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   array.length.times do |count|
     if array[count] == value_to_find
     
-    if else 
+    else if  
       array[count] == value_to_find
       return count
     end
