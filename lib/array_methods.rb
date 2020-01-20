73367ddf4a7632271c1a7735369859172cdeeb97
Patrick A <akolo@hotmail.com>
2020-01-20 19:48:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,12 +8,10 @@ def find_element_index(array, value_to_find)
   end 
   #array.index {|x| x == "value_to_find" }
   #binding.pry
-  
 end
 
 def find_max_value(array)
   array.length.max
-  nil
 end
 
 def find_min_value(array)
